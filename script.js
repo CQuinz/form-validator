2702393dd9cd184d37303dcf2a56c61cc1500d97
Christian <christian.quinlan36@gmail.com>
2021-03-10 20:55:54 +0000

    Creating js constants for html elements

---
+++ script.js
@@ -0,0 +1,9 @@
+// const name = document.querySelector('#name');
+// const phone =document.querySelector('#phone');
+// const email = document.querySelector('#email');
+// const website = document.querySelector('#website');
+const password1El = document.querySelector('#password1');
+const password2El = document.querySelector('#password2');
+const form = document.querySelector('#form');
+const messageContainer = document.querySelector('.message-container');
+const message = document.querySelector('#message');
