5a87c44cca84647257dd03a9755f5f9b64b336ff
Christian <christian.quinlan36@gmail.com>
2021-03-11 21:19:20 +0000

    Using the Constraint API to validate on form submit

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ const form = document.querySelector('#form');
 const messageContainer = document.querySelector('.message-container');
 const message = document.querySelector('#message');
 
+let isValid = false;
+
+const validateForm = ()=>{
+  // Using constraint API
+  isValid = form.checkValidity();
+  console.log(isValid);
+}
+
 const processFormData = (e)=>{
-  console.log(e);
+  e.preventDefault();
+  // Validate form
+  validateForm();
 }
 
 form.addEventListener('submit', processFormData);
