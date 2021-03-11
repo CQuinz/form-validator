10616c389c94d4c6d0d68ae957d081fc31919130
Christian <christian.quinlan36@gmail.com>
2021-03-11 21:37:55 +0000

    Adding validation to check if passwords match and to change stying if they dont

--- script.js
+++ script.js
@@ -9,11 +9,32 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.querySelector('#message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 const validateForm = ()=>{
   // Using constraint API
   isValid = form.checkValidity();
-  console.log(isValid);
+  // Style main message for an error
+  if(!isValid){
+    message.textContent = 'Please fill out all fields';
+    message.style.color='red';
+    messageContainer.gstyle.borderColor='red';
+  }
+  // Check if passwords match
+  if(password1El.value === password2El.value){
+    passwordsMatch = true;
+    password1El.style.borderColor='green';
+    password2El.style.borderColor='green';
+  }else{
+    passwordsMatch = false;
+    message.textContent='Make sure passwords match!';
+    message.style.color='red';
+    messageContainer.gstyle.borderColor='red';
+    password1El.style.borderColor='red';
+    password2El.style.borderColor='red'
+
+  }
+  
 }
 
 const processFormData = (e)=>{
